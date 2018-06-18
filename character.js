dad66a5d9cd624ae193f5397b6101332f2d76b95
Jacob Ochoa <Jacob.Ochoa@gmail.com>
2018-06-18 11:22:31 -0700

    xp braining

--- character.js
+++ character.js
@@ -1,8 +1,18 @@
-class xp {
-    constructor() {
-        this.defense
-        this.strength
-        this.speed
-        this.knowledge
+class experience {
+    constructor(defense, strength, agility, intellect) {
+        this.defense = defense
+        this.strength = strength
+        this.agility = agility
+        this.intellect = intellect
     }
+
+    increaseDefense() { }
+    increaseStrength() { }
+    increaseAgility() { }
+    increaseIntellect() { }
+
+    decreaseDefense() { }
+    decreaseStrength() { }
+    decreaseAgility() { }
+    decreaseIntellect() { }
 }
