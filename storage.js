55ccc663531d48f586e21510bbb56d695d5904be
jakobii <jacob.ochoa@gmail.com>
2018-06-18 23:37:45 -0700

    Create storage.js

---
+++ storage.js
@@ -0,0 +1,20 @@
+class storage {
+  constructor(){
+    this._guid
+    this._name
+    this._size
+    this._items
+    this._slots
+  }
+  
+  addItem(){}
+  findItem(){}
+  moveItem(){}
+  deleteItem(){}
+  
+  
+  switchItem(){}
+  sendItem(){}
+  
+  
+}
