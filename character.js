9a92429f598cae414c5b45359ea6f0a97eae6017
jakobii <jacob.ochoa@gmail.com>
2018-06-18 22:20:07 -0700

    Update character.js

--- character.js
+++ character.js
@@ -3,8 +3,8 @@ class character {
     this.guid //the characters globlally unique id
     this.name //the name that floats above the characters head
     
-    this.experience = xp // earned and leveled up xp.
-    this.stats = stats // this is xp plus any buffs
+    this.experienceEarned = xp // earned and leveled up xp.
+    this.experienceBuffed = stats // this is xp plus any buffs
     this.items = items // an array of item objects
   }
 }
