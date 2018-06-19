5d0a8e11378ff9fe78b9b0e80212f29ecb566b5b
jakobii <jacob.ochoa@gmail.com>
2018-06-18 22:09:04 -0700

    Create character.js
    tying out githubs built in editor...

---
+++ character.js
@@ -0,0 +1,10 @@
+class character {
+  constructor (){
+    this.guid //the characters globlally unique id
+    this.name //the name that floats above the characters head
+    
+    this.experience = xp // earned and leveled up xp.
+    this.stats = stats // this is xp plus any buffs
+    this.items = items // an array of item objects
+  }
+}
